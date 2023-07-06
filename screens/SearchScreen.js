3897ff2301dac51ef0055e7c7447debcc0e6a17c
Engin <engin48ect@gmail.com>
2023-07-06 17:45:15 +0300

    yemek tarifi

---
+++ screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { MEALS } from '../data/dummy-data';
+
+
+
+
+const SearchScreen = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Arama sayfası</Text>
+    </View>
+  );
+}
+
+export default SearchScreen;
